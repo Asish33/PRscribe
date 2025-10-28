7f6845f955ea59c0c3f0956d85956954b8bfc87b
Balaji Asish <balajiashish02@gmail.com>
2025-10-28 12:55:31 +0530

    add next-auth

---
+++ lib/authOptions.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
